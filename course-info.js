e192a3ad199ba51de9488a8a4d6d2730277223d9
loowaiyee11 <loowaiyee11@gmail.com>
2026-06-22 21:49:30 +0800

    Add files via upload

---
+++ course-info.js
@@ -0,0 +1,15 @@
+var courseInfo = {
+  // 你只需要改这里的资料。保留引号，不要删除逗号。
+  messageButtonText: "私讯「志工」了解详情",
+  messageLink: "https://wa.me/60167458279?text=你好，我想了解创伤知情催眠志工8月班",
+
+  registrationButtonText: "填写报名表",
+  registrationLink: "https://forms.gle/UCPJCfzTM5x7aTed7",
+
+  runningDetails: "日期与次数：8月，4堂课",
+  packageFaqNote: "2堂直播课，2堂视频课"
+};
+
+if (typeof window !== "undefined") {
+  window.courseInfo = courseInfo;
+}
